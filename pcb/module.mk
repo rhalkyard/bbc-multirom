94008c52f8e934cc75ebc30f121a98cd6d95e907
Richard Halkyard <rhalkyard@gmail.com>
2023-03-19 21:52:22 -0500

    Give up on PCB text variables for now

--- pcb/module.mk
+++ pcb/module.mk
@@ -8,15 +8,11 @@ PCB_FILES = pcb/$(NAME).kicad_pcb pcb/$(NAME).kicad_prl pcb/$(NAME).kicad_pro pc
 # since it takes a really long time to build some of its outputs.
 .NOTPARALLEL:
 
-# Workaround for https://gitlab.com/kicad/code/kicad/-/issues/14360 - PCB text
-# does not get updated with project variables
-pcb/$(NAME).kicad_pcb: version.txt
-	sed -i -e 's/(property "VERSION" ".*")/(property "VERSION" "$(VERSION)")/' pcb/$(NAME).kicad_pcb
-
 # Kibot generates its own makefile with all its build targets; generate it and
 # recurse into it for any target in the kibot output directory
 pcb/Makefile.kibot: pcb/default.kibot.yaml $(PCB_FILES)
 	kibot -d pcb/build -c $< -m $@ -b pcb/$(NAME).kicad_pcb
+# The Kibot makefile disables preflight checks, re-enable them
 	sed -i -e 's/-s all//' pcb/Makefile.kibot
 
 export VERSION
